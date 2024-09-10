b63ef55d841192db327747ffeafbda658e5b39d7
Augusto <augustoortolan02@gmail.com>
2024-09-10 19:51:36 -0300

    Assincronismo, async, await

--- aula02.js
+++ aula02.js
@@ -1,1 +1,18 @@
-console.log("Aula 02");
+function mostraInter() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Internacional");
+    }, 3000);
+  });
+}
+
+const mostraGremio = () => "Grêmio";
+
+async function main() {
+  let inter = await mostraInter();
+  console.log(inter);
+  let gremio = mostraGremio();
+  console.log(gremio);
+}
+
+main();
